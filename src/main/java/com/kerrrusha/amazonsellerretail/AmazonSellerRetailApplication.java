c897fdb64423b09b6809e1514ac5d29919bab568
Kirill Koval <brcyrill2@gmail.com>
2024-02-28 20:46:17 +0200

    init commit

---
+++ src/main/java/com/kerrrusha/amazonsellerretail/AmazonSellerRetailApplication.java
@@ -0,0 +1,13 @@
+package com.kerrrusha.amazonsellerretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonSellerRetailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonSellerRetailApplication.class, args);
+    }
+
+}
